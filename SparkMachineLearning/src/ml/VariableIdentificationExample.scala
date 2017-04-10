776e7d2f3fb7e710bc58dfbcffa86dece03e052b
suria <suria.r.asai@gmail.com>
2017-04-10 01:23:47 -0700

    VI

--- SparkMachineLearning/src/ml/VariableIdentificationExample.scala
+++ SparkMachineLearning/src/ml/VariableIdentificationExample.scala
@@ -3,13 +3,12 @@
 /**
  * @author training
  */
+package ml
+
 import org.apache.spark._
-import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.DataFrameNaFunctions
 import org.apache.spark.sql.types._
-import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
 
 object VariableIdentificationExample {
   
