a9a45e500fa206644fc831ee867ad8c71abeb7bb
suria <suria.r.asai@gmail.com>
2017-04-10 00:43:38 -0700

    First Commit

---
+++ SparkMachineLearning/src/VariableIdentificationExample.scala
@@ -0,0 +1,16 @@
+
+
+/**
+ * @author training
+ */
+import org.apache.spark._
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.DataFrameNaFunctions
+import org.apache.spark.sql.types._
+import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
+
+object VariableIdentificationExample {
+  
+}
